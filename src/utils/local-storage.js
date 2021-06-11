11828cec6a9a226f33dc3ebee8539410d5ee720b
david-huynh <huynh.david.work@gmail.com>
2021-06-11 01:09:35 -0400

    formatting

--- src/utils/local-storage.js
+++ src/utils/local-storage.js
@@ -1,15 +1,14 @@
 export const setToLS = (key, value) => {
-    if (typeof window !== 'undefined') {
-        localStorage.setItem(key, JSON.stringify(value));
+  if (typeof window !== "undefined") {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+};
+
+export const getFromLS = (key) => {
+  if (typeof window !== "undefined") {
+    const value = localStorage.getItem(key);
+    if (value) {
+      return JSON.parse(value);
     }
-}
-  
-export const getFromLS = key => {
-    if (typeof window !== 'undefined') {
-        const value = localStorage.getItem(key);
-        if (value) {
-            return JSON.parse(value);
-        }
-    }
-    
-}
+  }
+};
