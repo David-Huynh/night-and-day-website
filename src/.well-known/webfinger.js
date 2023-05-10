3ce1c8973cd03c645e7d0307b49c3d44cea760fd
David <huynh.david.work@gmail.com>
2023-05-09 21:08:43 -0400

    test

--- src/.well-known/webfinger.js
+++ src/.well-known/webfinger.js
@@ -1,4 +1,17 @@
 export default async function handler(req, res) {
-    const rel = req.rel;
-    return rel;
+    const redirectMap = [
+        'david@dhuynh.ca',
+        'ada@dhuynh.ca',
+        'tin@dhuynh.ca',
+        'anh@dhuynh.ca',
+      ];
+    const resourceKey = req.resource;
+    const email = resourceKey.replace("acct:", "");
+    if (redirectMap.includes(email)) {
+        const jsonData = '{"subject":"acct:' + email + '","links":[{"rel":"http://openid.net/specs/connect/1.0/issuer","type":"text/html","href":"https://auth.dhuynh.ca"}]}';
+        res.json(jsonData);
+    } else {
+        res.status(400)
+    }
+    
 }
